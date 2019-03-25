e5260388c6351e7aca94c08854288e03fb231266
Mahlatse <31064845+Mahlatse32@users.noreply.github.com>
2019-03-25 17:05:53 +0200

    the progress so long. .

---
+++ Navigation/Navigation/ContactPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Navigation
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ContactPage : ContentPage
+	{
+		public ContactPage ()
+		{
+			InitializeComponent ();
+
+            list_view.ItemsSource = new List<Contact>
+            {
+                new Contact { Name = "Mahlatse", ImageUrl = "http://lorempixel.com/100/100/people/1", Status = "nja" },
+				new Contact { Name = "Emma", ImageUrl = "http://lorempixel.com/100/100/people/2", Status = "Hey, let's talk!" }
+            };
+		}
+
+        async void List_view_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+                return;
+
+            var contact = e.SelectedItem as Contact;
+            await Navigation.PushAsync(new ContactDetailPage(contact));
+            list_view.SelectedItem = null;
+        }
+    }
+}
---
+++ Navigation/Navigation/IntroductionPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Navigation
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class IntroductionPage : ContentPage
+	{
+		public IntroductionPage ()
+		{
+			InitializeComponent ();
+		}
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
